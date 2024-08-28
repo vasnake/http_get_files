17ddacbb62d4533576eda37bb6ef9b1f86a24d62
Valentin Fedulov <vasnake@gmail.com>
2024-08-28 13:13:19 +0300

    refactored

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 
 Very simple Go program, enumerate files in given directory and serve them via HTTP GET
 
-Motivation: I tried to setup VCL streaming (server) on my win11 box,
-just to have an ability to watch a few TV series on my phone.
-Unsuccessfully. After a couple of hours I said to myself - "fuck it, all I need is a primitive HTTP server, nothing more".
+Motivation: I tried to setup VLC streaming (server) on my win11 box,
+just to have an ability to watch a few TV series (files on my win11 box) on my phone.
+Well, this time VLC magic dindn't work for me.
+After a couple of hours I said to myself - "fuck it, all I need is a primitive HTTP server, nothing more".
 And there it is, a primitive HTTP server.
 
 I build it and run in WSL2 under Win11 env.
 
 Build
 ```s
-pushd /mnt/c/Users/valik/data/github/http_get_files/
+pushd /mnt/c/Users/vlk/data/github/http_get_files/
 PATH=${HOME}/go/bin:${PATH}
 
 go mod init http_get_files
@@ -41,6 +42,3 @@ netsh interface ip show address
 
 ```
 snippets
-
-Example: `http://192.168.1.6:8080/1` will get you file `c:\Users\valik\Downloads\video_file_foo.avi`
-if you create the mapping in code, from `1` to `c:\Users\valik\Downloads\video_file_foo.avi`.
--- main.go
+++ main.go
@@ -2,31 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"net/http"
+	"os"
+	"path/filepath"
+	"slices"
+	"time"
 )
 
-func main() {
-	const listenOn = ":8080"
-	const fnameKey = "fname"
+// parameters
+const listenPort = "8080"
+const serveDirectoryPath = "/mnt/c/Users/valik/Downloads/Reacher.S02.WEB-DLRip.LF"
 
-	var mapping = map[string]string{
-		"1": "/mnt/c/Users/valik/Downloads/Reacher.S02.WEB-DLRip.LF/Reacher.S02E01.WEB-DLRip.RGzsRutracker.avi",
-		"2": "/mnt/c/Users/valik/Downloads/Reacher.S02.WEB-DLRip.LF/Reacher.S02E02.WEB-DLRip.RGzsRutracker.avi",
-		"3": "/mnt/c/Users/valik/Downloads/Reacher.S02.WEB-DLRip.LF/Reacher.S02E03.WEB-DLRip.RGzsRutracker.avi",
-		"4": "/mnt/c/Users/valik/Downloads/Reacher.S02.WEB-DLRip.LF/Reacher.S02E04.WEB-DLRip.RGzsRutracker.avi",
-		"5": "/mnt/c/Users/valik/Downloads/Reacher.S02.WEB-DLRip.LF/Reacher.S02E05.WEB-DLRip.RGzsRutracker.avi",
-		"6": "/mnt/c/Users/valik/Downloads/Reacher.S02.WEB-DLRip.LF/Reacher.S02E06.WEB-DLRip.RGzsRutracker.avi",
-		"7": "/mnt/c/Users/valik/Downloads/Reacher.S02.WEB-DLRip.LF/Reacher.S02E07.WEB-DLRip.RGzsRutracker.avi",
-		"8": "/mnt/c/Users/valik/Downloads/Reacher.S02.WEB-DLRip.LF/Reacher.S02E08.WEB-DLRip.RGzsRutracker.avi",
+func main() {
+	var files = dirList(serveDirectoryPath)
+	if len(files) == 0 {
+		panicOnError("No files to serve", fmt.Errorf("len(files) == 0"))
 	}
+	slices.Sort(files)
+
+	var mapping = buildMapping(files)
+
+	var listenOn = fmt.Sprintf(":%v", listenPort)
+
+	var err = serve(mapping, listenOn)
+	panicOnError("Serve failed", err)
+}
+
+func serve(mapping map[string]string, listenOn string) error {
+	const fnameRequestKey = "fname"
 
 	var handler = func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Processing %v ...", r.RequestURI)
+		log("Processing URI ", r.RequestURI)
 
-		fname := r.PathValue(fnameKey)
+		fname := r.PathValue(fnameRequestKey)
 		fpath, exists := mapping[fname]
 
 		if exists {
+			log("Serving file ", fpath)
 			http.ServeFile(w, r, fpath)
 		} else {
 			http.Error(w, fmt.Sprintf("No mapping from name '%v'", fname), http.StatusNotFound)
@@ -34,9 +47,102 @@ func main() {
 	}
 
 	var mux *http.ServeMux = http.NewServeMux()
-	mux.HandleFunc(fmt.Sprintf("GET /{%v}", fnameKey), handler)
-	http.Handle("/", mux)
+	mux.HandleFunc(fmt.Sprintf("GET /{%v}", fnameRequestKey), handler)
+	http.Handle("/", mux) // set default handler
 
-	fmt.Printf("Starting server at %v\n", listenOn)
-	http.ListenAndServe(listenOn, nil)
+	log("Starting server at ", listenOn)
+	return http.ListenAndServe(listenOn, nil) // nil: default handler
+}
+
+func buildMapping(names []string) map[string]string {
+	var mapping = make(map[string]string, len(names))
+	for idx, name := range names {
+		var key = fmt.Sprintf("%v", idx+1)
+		mapping[key] = name
+		log("added mapping ", key, name)
+	}
+	return mapping
+}
+
+func dirList(path string) []string {
+	var cleanPath string = cleanPath(path)
+
+	entries, err := readDir(cleanPath)
+	panicOnError("Can't read files from "+path, err)
+
+	var lst = make([]string, 0, len(entries))
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			log("skip directory ", entry.Name())
+		} else {
+			filePath := joinPathElems(cleanPath, entry.Name())
+			lst = append(lst, filePath)
+			log("file added: ", filePath)
+		}
+	}
+
+	return lst
+}
+
+func cleanPath(path string) string {
+	return filepath.Clean(path)
+}
+
+func joinPathElems(e1, e2 string) string {
+	var joined = filepath.Join(e1, e2)
+	return joined
+}
+
+func readDir(path string) ([]fs.DirEntry, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	entries, err := file.ReadDir(0)
+	if err != nil {
+		return nil, err
+	}
+
+	return entries, nil
+}
+
+func panicOnError(msg string, err error) {
+	if err != nil {
+		panic(msg + ": " + err.Error())
+	}
+}
+
+// log writes message to standard output (fmt.Println). Message combined from prefix msg and slice of arbitrary values
+func log(msg string, xs ...any) {
+	var line = buildMessage(msg, xs...)
+	fmt.Println(line)
+}
+
+func buildMessage(msg string, xs ...any) string {
+	var line = ts() + ": " + msg // TODO: use buffer, not string concat
+
+	for _, x := range xs {
+		// https://pkg.go.dev/fmt
+		// line += fmt.Sprintf("%T(%v); ", x, x) // type(value)
+		line += fmt.Sprintf("%#v; ", x) // repr
+	}
+
+	return line
+}
+
+// ts returns current timestamp in RFC3339 with milliseconds
+func ts() string {
+	/*
+		https://pkg.go.dev/time#pkg-constants
+		https://stackoverflow.com/questions/35479041/how-to-convert-iso-8601-time-in-golang
+	*/
+	const (
+		RFC3339      = "2006-01-02T15:04:05Z07:00"
+		RFC3339Milli = "2006-01-02T15:04:05.000Z07:00"
+	)
+	return time.Now().UTC().Format(RFC3339Milli)
 }
